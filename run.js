fbbf2f2e3bfb71c3c8ae03a9f9834c30c129358f
addshore <addshorewiki@gmail.com>
2024-10-08 23:02:20 +0100

    Set wikibase.cloud host automatically

--- run.js
+++ run.js
@@ -59,6 +59,12 @@ const worldEdit = WBEdit({
 const queue = new PQueue({concurrency: CONCURRENCY});
 const ee = new EventEmitter();
 
+// get the first arg to run.js
+const scriptFilter = process.argv[2]
+if (scriptFilter != undefined) {
+    console.log(`🚀 Running with script filter: ${scriptFilter}`)
+}
+
 // Queue an initial lookup of live wikibase.world wikis
 queue.add(async () => {
     const sparql = `
@@ -74,6 +80,9 @@ queue.add(async () => {
     let results = minimizeSimplifiedSparqlResults(simplifySparqlResults((raw)))
     // Emit a 'world.wikis' event for each wiki found
     results.forEach(async (result) => {
+        if (scriptFilter != undefined && !result.site.includes(scriptFilter)) {
+            return
+        }
         ee.emit('world.wikis', result)
     });
 });
@@ -99,6 +108,21 @@ ee.on('world.wikis', (result) => {
 });
 
 ee.on('world.wikis.200', ({ wiki, response }) => {
+    // If we got a 200, then it is online, so P13 can be online Q54
+
+    // If the domain ends in .wikibase.cloud, then ensure P2 (Host) -> Q8 (Wikibase.cloud) on the world item
+    if (wiki.site.endsWith('.wikibase.cloud')) {
+        queue.add(async () => {
+            const url = world.getEntities({ids: [ wiki.item ]})
+            const { entities } = await fetchuc(url, { headers: HEADERS }).then(res => res.json())
+            const simpleClaims = simplifyClaims(entities[wiki.item].claims)
+            if (!simpleClaims.P2 || simpleClaims.P2[0] !== 'Q8') {
+                console.log(`🖊️ Adding P2 (Host) claim to ${wiki.item} for ${wiki.site}`)
+                ee.emit('world.editRequest.claimEnsure', { data: {id: wiki.item, property: 'P2', value: 'Q8'}, requestConfig: { summary: `Add [[Property:P2]] claim for [[Item:Q8]] based on [[Property:P1]] of ${wiki.site}` } })
+            }
+        });
+    }
+
     // We can try to normalize the URL if it is a Main_Page
     if (wiki.site.includes('/wiki/Main_Page')) {
         queue.add(async () => {
@@ -132,5 +156,33 @@ ee.on('world.editRequest.claimUpdate', ({ data, requestConfig }) => {
     queue.add(async () => {
         console.log(`🖊️ Updating claim for ${data.id} with ${data.property} from ${data.oldValue} to ${data.newValue}`)
         worldEdit.claim.update(data, requestConfig)
+    });
+});
+
+ee.on('world.editRequest.claimEnsure', ({ data, requestConfig }) => {
+    queue.add(async () => {
+        // Get the entity from data.id
+        const url = world.getEntities({ids: [ data.id ]})
+        const { entities } = await fetchuc(url, { headers: HEADERS }).then(res => res.json())
+        const simpleClaims = simplifyClaims(entities[data.id].claims)
+        // TODO handle multiple claims of the property?
+        if (simpleClaims[data.property] && simpleClaims[data.property].length > 1) {
+            console.log(`❌ The claim for ${data.id} with ${data.property} has more than 1 value`)
+            return
+        }
+        if (simpleClaims[data.property] && simpleClaims[data.property][0] === data.value) {
+            console.log(`✅ The claim for ${data.id} with ${data.property} to ${data.value} already exists`)
+            return
+        }
+
+        console.log(`✅ The claim for ${data.id} with ${data.property} to ${data.value} does not exist`)
+        ee.emit('world.editRequest.claimCreate', { data: {id: data.id, property: data.property, value: data.value}, requestConfig: requestConfig })
+    });
+});
+
+ee.on('world.editRequest.claimCreate', ({ data, requestConfig }) => {
+    queue.add(async () => {
+        console.log(`🖊️ Creating claim for ${data.id} with ${data.property} to ${data.value}`)
+        worldEdit.claim.create(data, requestConfig)
     });
 });
